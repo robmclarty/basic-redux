ec4e563a397a6354ac6feb0e60bc61c842f5a527
slode <rob.m@myplanet.com>
2019-11-11 18:19:29 -0500

    combine middleware with redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, compose } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 
+import apiMiddleware from './middleware/api_middleware'
 import appReducer from './reducers'
 import AppContainer from './containers/app_container'
 
-const store = createStore(appReducer)
+// If Chrome devToolsExtension exists, hook it up as middleware.
+const devTools = window.devToolsExtension ?
+  window.devToolsExtension() :
+  f => f
+
+// Combine middlewares together.
+const middlewares = compose(
+  applyMiddleware(apiMiddleware),
+  devTools
+)
+
+// Create store from app's reducers combined with middlewares.
+const store = createStore(appReducer, middlewares)
 
 render(
   <Provider store={store}>
