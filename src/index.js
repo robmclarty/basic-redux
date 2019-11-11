21e3410d74d072f6481d0c45a393fe1fc0183533
slode <rob.m@myplanet.com>
2019-11-11 15:55:04 -0500

    add missing slash to close single element tag for <App />

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { render } from 'react-dom'
 
 import App from './containers/app_container'
 
-render(<App>, document.getElementById('app-root'))
+render(<App />, document.getElementById('app-root'))
