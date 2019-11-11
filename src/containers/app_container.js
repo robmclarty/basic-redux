0bdce9234d2805d9af8226d061123a7fb9148b11
slode <rob.m@myplanet.com>
2019-11-11 18:39:25 -0500

    update app container to use new Notification component

--- src/containers/app_container.js
+++ src/containers/app_container.js
@@ -1,25 +1,44 @@
 import React from 'react'
 import { connect } from 'react-redux'
+
+import { fetchRates } from '../actions'
 import Button from '../components/button'
 import RateTable from '../components/rate_table'
-import { fetchRates } from '../actions'
+import Notification from '../components/notification'
 
 const mapStateToProps = state => ({
-  rates: state.rates
+  rates: state.rates,
+  notificationIsVisible: state.notifications.isVisible,
+  notificationMessage: state.notifications.message,
+  notificationStatus: state.notifications.status
 })
 
 const mapDispatchToProps = dispatch => ({
-  onButtonPress: () => dispatch(fetchRates())
+  onButtonPress: () => dispatch(fetchRates()),
+  onResetNotification: () => dispatch(resetNotification())
 })
 
-const AppContainer = ({ rates, onButtonPress }) => (
-  <div>
-    <Button
-      label="Fetch Rates"
-      onPress={onButtonPress} />
+const AppContainer = ({
+  rates,
+  notificationIsVisible,
+  notificationMessage,
+  notificationStatus,
+  onButtonPress
+}) => (
+  <main>
+    <Notification
+        isVisible={notificationIsVisible}
+        status={notificationStatus}
+        message={notificationMessage}
+        onReset={onResetNotification} />
 
+    <h2>Currency Rates</h2>
     <RateTable rates={rates} />
-  </div>
+
+    <Button
+        label="Fetch Rates"
+        onPress={onButtonPress} />
+  </main>
 )
 
 export default connect(mapStateToProps, mapDispatchToProps)(AppContainer)
