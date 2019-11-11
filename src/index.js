cddce04c0436715f3ade675f02d6355863a18c80
slode <rob.m@myplanet.com>
2019-11-11 16:21:57 -0500

    wrap app in provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore, compose } from 'redux'
-import appReducer from './reducers'
+import { Provider } from 'react-redux'
 
+import appReducer from './reducers'
 import AppContainer from './containers/app_container'
 
 const store = createStore(appReducer)
 
-render(<AppContainer />, document.getElementById('app-root'))
+render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.getElementById('app-root')
+)
