b7e511a23db3d137cc79988067f55527245c5beb
slode <rob.m@myplanet.com>
2019-11-11 17:52:40 -0500

    debug mapRatesToState(); reducer assumption was incorrect

--- src/helpers/rate_helper.js
+++ src/helpers/rate_helper.js
@@ -1,6 +1,9 @@
 // Take updated rates and combine them with an existing state.
 export const mapRatesToState = (state, rates) => {
   return Object.keys(rates).reduce((newState, key) => {
-    newState[key] = rates[key]
-  }, [...state])
+    return {
+      ...newState,
+      [key]: rates[key]
+    }
+  }, {...state})
 }
