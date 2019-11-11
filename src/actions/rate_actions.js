36b1725615e804a7fae2cc11cbf30fa6c86c6017
slode <rob.m@myplanet.com>
2019-11-11 17:30:29 -0500

    create temporary action data flow code to be used until middleware is implemented

--- src/actions/rate_actions.js
+++ src/actions/rate_actions.js
@@ -4,12 +4,24 @@ import {
   FETCH_RATES_FAIL
 } from '../constants/action_types'
 
+const sampleFetchRates = {
+  "CAD": 1.273,
+  "USD": 1.0111,
+  "SEK": 10.7025
+}
+
 // Fetch Rates
 // -----------
 export const fetchRates = () => (dispatch, callApi) => {
   dispatch(fetchRatesPending())
 
-  return callApi({ url: ratesUrl, method: 'GET' })
+  // return callApi({ url: ratesUrl, method: 'GET' })
+  //   .then(res => dispatch(fetchRatesSuccess(res.rates)))
+  //   .catch(err => dispatch(fetchRatesFail(err)))
+
+  // REFACTOR: This is only temporary to test out the core action data flow.
+  // Uncomment the above code when middleware has been implemented.
+  return Promise.resolve({ rates: sampleFetchRates })
     .then(res => dispatch(fetchRatesSuccess(res.rates)))
     .catch(err => dispatch(fetchRatesFail(err)))
 }
