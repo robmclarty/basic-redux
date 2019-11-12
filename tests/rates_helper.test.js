80ca4359b3e18042327115692d399bb5e8fb36ab
slode <rob.m@myplanet.com>
2019-11-12 10:21:53 -0500

    group test case in describe block

--- tests/rates_helper.test.js
+++ tests/rates_helper.test.js
@@ -1,16 +1,19 @@
 import { mapRatesToState } from '../src/helpers/rate_helper'
 
-const sampleRates = {
-  "CAD": 1.4561,
-  "USD": 1.1034,
-  "JPY": 120.72,
-  "GBP": 0.86158
-}
+describe('rates_helper', () => {
+  const sampleRates = {
+    "CAD": 1.4561,
+    "USD": 1.1034,
+    "JPY": 120.72,
+    "GBP": 0.86158
+  }
 
-test('new rates are added to the store', () => {
-  const state = {}
-  const rates = sampleRates
-  const newState = mapRatesToState(state, rates)
+  test('new rates are added to the store', () => {
+    const state = {}
+    const rates = sampleRates
+    const newState = mapRatesToState(state, rates)
 
-  expect(newState['CAD']).toBe(1.4561)
+    expect(Object.keys(newState).length).toBe(4)
+    expect(newState['CAD']).toBe(1.4561)
+  })
 })
